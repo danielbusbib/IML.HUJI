3639f3a6b13f6939dec8788ba41949d4e85ffd87
danielbusbib <daniel.busbib@gmail.com>
2022-04-09 22:13:40 +0300

    fix ex3

--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -36,7 +36,11 @@ def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: b
     -------
     Misclassification of given predictions
     """
-    raise NotImplementedError()
+    if normalize:
+        return np.mean((y_true - y_pred) != 0)
+
+    # count errors
+    return np.sum((y_true - y_pred) != 0)
 
 
 def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
@@ -54,7 +58,7 @@ def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     -------
     Accuracy of given predictions
     """
-    raise NotImplementedError()
+    return np.mean((y_true - y_pred) == 0)
 
 
 def cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
