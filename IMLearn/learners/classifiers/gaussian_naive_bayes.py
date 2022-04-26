d37b1a0177a9d09822daebf428b179bee267c972
danielbusbib <daniel.busbib@gmail.com>
2022-04-27 00:22:34 +0300

    fix GNB

--- IMLearn/learners/classifiers/gaussian_naive_bayes.py
+++ IMLearn/learners/classifiers/gaussian_naive_bayes.py
@@ -153,17 +153,3 @@ class GaussianNaiveBayes(BaseEstimator):
         """
         from ...metrics import misclassification_error
         return misclassification_error(y, self._predict(X))
-
-
-from sklearn.naive_bayes import GaussianNB
-
-clf = GaussianNB()
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-y = np.array([1, 1, 1, 2, 2, 2])
-f = GaussianNaiveBayes()
-f._fit(X, y)
-clf.fit(X, y)
-print(clf.theta_)
-print(f.mu_)
-print(clf.predict_proba([[-0.8, -1]]))
-print(f.likelihood(np.array([[-0.8, -1]])))
